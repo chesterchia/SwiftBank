fcc9567a45155b5c9d795f25c76d6331b087b423
Germaine <tanlixuangermaine@gmail.com>
2024-04-01 03:29:46 +0800

    add useContext

--- client/src/components/DisplayBranch.js
+++ client/src/components/DisplayBranch.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React,{useState, useEffect, useContext} from 'react';
 
 const DisplayBranch = () =>{
     const [AllBranches,setBranches] = useState([]);
--- client/src/components/FormTransaction.js
+++ client/src/components/FormTransaction.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React,{useState, useEffect, useContext} from 'react';
 import { useHistory } from 'react-router-dom';
 
 const FormTransaction = ()=>{
